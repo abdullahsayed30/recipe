bb46f26c03c8baeeebb22b9f16123395c59a48c8
Abdullah <abdullahsayed30@gmail.com>
2022-06-02 13:08:48 +0200

    Implement Index Page + Controller. closes #1, #2.

---
+++ src/main/java/com/app/recipe/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.app.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+
+        return "index";
+    }
+}
